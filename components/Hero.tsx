7bed16b31df14fd74c412868415e63137d167233
mustafa <msolmaz83@gmail.com>
2024-01-23 14:57:02 +0100

    images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,15 @@
 import Particle from "./Particle"
+import TextEffect from "./TextEffect"
 
 
 const Hero = () => {
   return (
     <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
         <Particle/>
+        <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+          <h1 className="text-[35px] md:text-[50px] text-white font-bold">Hi ,I'M <span className="text-yellow-400">Muhammet</span></h1>
+          <TextEffect/>
+        </div>
     </div>
   )
 }
