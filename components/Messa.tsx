3dbfe9cce1fcc0239199b76015112757dc9e7122
mustafa <msolmaz83@gmail.com>
2024-01-25 01:01:47 +0100

    pdf

--- components/Messa.tsx
+++ components/Messa.tsx
@@ -4,7 +4,7 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp'
 const Messa = () => {
   return (
     <FloatingWhatsApp 
-    accountName='user'
+    accountName='MuhammetSOLMAZ'
     phoneNumber="+4917632798189" 
     avatar='https://png.pngtree.com/thumb_back/fw800/background/20230612/pngtree-male-avatar-image-in-the-circle-image_2908803.jpg'/>
   )
