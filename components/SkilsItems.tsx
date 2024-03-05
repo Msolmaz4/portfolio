83b608fd0f8caf8fd5e9d2f8db05d78028f31542
mustafa <msolmaz83@gmail.com>
2024-03-05 21:35:04 +0100

    inner

--- components/SkilsItems.tsx
+++ components/SkilsItems.tsx
@@ -1,16 +1,17 @@
 interface Props {
     title:string,
     year:string,
+    inner:string
 }
 
-const SkilsItems = ({title,year}:Props) => {
+const SkilsItems = ({title,year,inner}:Props) => {
   return (
-    <div className="mb-[4rem] md:mb-[8rem]">
+    <div className="mb-[4rem] md:mb-[8rem] text-justify">
         <span className="px-[2rem] text-[#55e6a5] py-[0.9rem] font-bold text-[18px] border-[#556e5] border-[2px]">
             {year}
         </span>
         <h1 className="mt-[2rem] uppercase mb-[1rem] text-[20px] sm:text-[25px] md:text-[34px] text-white">{title} </h1>
-        <p className="text-white font-normal w-[80%] text-[17px] opacity-80">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum quae voluptas omnis accusantium ipsum accusamus, sunt consequuntur, commodi assumenda hic maxime et eaque excepturi nemo quos, saepe beatae! Obcaecati cumque voluptates doloremque iste.</p>
+        <p className="text-white font-normal w-[80%] text-[17px] opacity-80">  {inner}.</p>
     </div>
   )
 }
