d3939d425eff5b77a180de35a89d365ea259f5d9
mustafa <msolmaz83@gmail.com>
2024-03-05 18:57:30 +0100

    eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,7 @@ module.exports = {
       },
     },
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+
   plugins: [],
 }
 
